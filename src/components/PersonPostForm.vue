1a0aaf8690bbf0752fb813eb3a45c36d6fd5a301
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2022-03-26 14:49:25 +0900

    cssのクラスを追加

--- src/components/PersonPostForm.vue
+++ src/components/PersonPostForm.vue
@@ -3,11 +3,11 @@
 <template>
   <div class="form-container">
     <div class="input-container">
-      <div>
+      <div class="input-column">
         <span>name:</span>
         <input class="input" />
       </div>
-      <div>
+      <div class="input-column">
         <span>age:</span>
         <input class="input" />
       </div>
@@ -30,6 +30,15 @@
 span {
   font-size: 20px;
   font-weight: bold;
+}
+.input-column {
+  width: 200px;
+  display: flex;
+  justify-content: space-between;
+}
+input {
+  width: 120px;
+  margin-bottom: 8px;
 }
 .input-container {
   display: flex;
