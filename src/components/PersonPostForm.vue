b7c1092c6b41fadced4737b2659c13462609ee11
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2022-04-02 12:49:42 +0900

    追加

--- src/components/PersonPostForm.vue
+++ src/components/PersonPostForm.vue
@@ -1,18 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, defineEmits } from "vue";
+
+const inputtingName = ref<string>("");
+const inputtingAge = ref<number>(0);
+
+const emit = defineEmits(["register"]);
+
+const register = () => {
+  const person = {
+    id: Math.random(),
+    name: inputtingName.value,
+    age: inputtingAge.value,
+  };
+  emit("register", person);
+};
+</script>
 
 <template>
   <div class="form-container">
     <div class="input-container">
       <div class="input-column">
         <span>name:</span>
-        <input class="input" />
+        <input class="input" v-model="inputtingName" />
       </div>
       <div class="input-column">
         <span>age:</span>
-        <input class="input" />
+        <input class="input" v-model="inputtingAge" type="number" />
       </div>
     </div>
-    <button class="register-button">register</button>
+    <button v-on:click="register" class="register-button">register</button>
   </div>
 </template>
 
