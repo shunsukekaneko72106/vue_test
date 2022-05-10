488e790dda52d1deb5c62768d8aa110bd5f4dd87
shunsukekaneko72106 <61620829+shunsukekaneko72106@users.noreply.github.com>
2022-05-10 21:26:47 +0900

    変更

--- src/components/UnderstandingTypeScript.vue
+++ src/components/UnderstandingTypeScript.vue
@@ -1,11 +1,35 @@
 <script setup lang="ts">
+/**
+ * デコレーター
+ */
+function autobind(
+  _target: any, //ターゲット
+  _methodName: string, //プロパティ名
+  descriptor: PropertyDescriptor //プロパティディスクリプター
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 //プロジェクトクラス
 class ProjectInput {
+  //thisの型設定
   templeteElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElemtnt: HTMLInputElement;
+  setumeiInputElemtnt: HTMLInputElement;
+  dateInputElemtnt: HTMLInputElement;
+
+  //コンストラクター（クラスが呼ばれた場合に実行する）
   constructor() {
-    console.log("hogehoge");
     //テンプレートへの参照
     this.templeteElement = document.getElementById(
       "projectinput"
@@ -13,10 +37,68 @@ class ProjectInput {
     this.hostElement = document.getElementById("appunder") as HTMLDivElement;
     const importedNode = document.importNode(this.templeteElement, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    //CSSにIDを付与するため
     this.element.id = "user-input";
+    //inputタグを参照
+    this.titleInputElemtnt = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.setumeiInputElemtnt = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.dateInputElemtnt = this.element.querySelector(
+      "#manday"
+    ) as HTMLInputElement;
+
+    this.configure();
     this.attch();
   }
-  private attch() {
+  /**
+   * inputの値を取得する
+   * 戻り値は「タプル」か「なし」かを指定する
+   */
+  private getUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElemtnt.value;
+    const enteredSetumei = this.setumeiInputElemtnt.value;
+    const enteredDate = this.dateInputElemtnt.value;
+
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredSetumei.trim().length === 0 ||
+      enteredDate.trim().length === 0
+    ) {
+      alert("入力値がただしくありません");
+    } else {
+      return [enteredTitle, enteredSetumei, +enteredDate];
+    }
+  }
+
+  /**
+   * submitHandler
+   * @param event
+   */
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.getUserInput();
+    //配列かどうかを判定しないとエラーが出る（反復子プロトコルは、値の並び（有限でも無限でも）を生成）
+    if (Array.isArray(userInput)) {
+      const [title, setumei, date] = userInput;
+      console.log(title, setumei, date);
+    }
+  }
+
+  /**
+   * 送信ボタンの処理
+   */
+  private configure(): void {
+    //submitが呼ばれるたびにthis.submitHandlerを呼ぶ
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+  /**
+   * 最後のDiv要素に値を追加する
+   */
+  private attch(): void {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
